821f9e458a4db1d9642c77ac1248a8e57ddf7929
Ty Eggen <tycho.eggen@gmail.com>
2017-02-01 01:24:03 -0800

    working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,12 @@
 console.log('This would be the main JS file.');
 $( document ).ready( function() {
 
-	var xhr = $.ajax({
-		url: "/status.json",
-		success: function(response) {
-			$( "#last_updated" ).text( $.timeago( xhr.getResponseHeader("Last-Modified"));
-			$( "#alive_status" ).html( response.data.state );
-    		}
+	String.prototype.capitalizeFirstLetter = function() {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+
+	$.get( "status.json", function( response ) {
+		$( "#last_updated" ).text( $.timeago( response.last_seen ) );
+		$( "#alive_status" ).html( response.state );
 	});
 });
